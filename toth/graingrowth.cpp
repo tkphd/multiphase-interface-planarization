a4ce4f39471255d17c07b67e7285bb768501e6f1
Trevor Keller <trevor.keller@nist.gov>
2015-12-15 11:50:24 -0500

    keep working on indices

--- toth/graingrowth.cpp
+++ toth/graingrowth.cpp
@@ -34,8 +34,8 @@ void generate(int dim, const char* filename)
 	if (dim==2) {
 		MMSP::grid<2,vector<double> > grid(3,0,128,0,128);
 
-		double lo = 0.0; //1.0e-5;
-		double hi = 1.0; // - 2.0*lo;
+		double lo = 0.0;// 1.0e-5;
+		double hi = 1.0;// 1.0 - 2.0*lo;
 
 		#ifndef MPI_VERSION
 		#pragma omp parallel for
@@ -43,7 +43,7 @@ void generate(int dim, const char* filename)
 		for (int n=0; n<nodes(grid); n++) {
 			vector<int> x = position(grid,n);
 
-			if (std::pow(x[0]-64,2)+std::pow(x[1]-64,2) < 625) {
+			if (std::pow(x[0]-64,2.0)+std::pow(x[1]-64,2.0) < 625) {
 				grid(n)[0] = hi;
 				grid(n)[1] = lo;
 				grid(n)[2] = lo;
@@ -70,7 +70,7 @@ void generate(int dim, const char* filename)
 	}
 
 	if (dim==3) {
-		MMSP::grid<3,vector<double> > grid(0,0,64,0,64,0,64);
+		MMSP::grid<3,vector<double> > grid(3,0,64,0,64,0,64);
 
 		#ifndef MPI_VERSION
 		#pragma omp parallel for
@@ -81,14 +81,18 @@ void generate(int dim, const char* filename)
 			if (x[0]<16) {
 				if (x[1]<32) grid(n)[1] = 1.0;
 				else grid(n)[2] = 1.0;
+				grid(n)[0] = 0.0;
 			}
 			else if (x[0]>48) {
 				if (x[1]<32) grid(n)[1] = 1.0;
 				else grid(n)[2] = 1.0;
+				grid(n)[0] = 0.0;
+
 			}
 			else {
-				if (x[1]<16 or x[1]>48) grid(n)[0] = 1.0;
+				if (x[1]<16 || x[1]>48) grid(n)[0] = 1.0;
 				else grid(n)[0] = 1.0;
+				grid(n)[1] = 0.0;
 			}
 		}
 
@@ -96,48 +100,15 @@ void generate(int dim, const char* filename)
 	}
 }
 
-template <int dim, typename T> vector<T> upside_grad(const grid<dim, T>& GRID, const vector<int>& x)
-{
-    vector<T> gradient(dim);
-    vector<int> s = x;
-
-    for (int i=0; i<dim; i++) {
-        s[i] += 1;
-        const T& yh = GRID(s);
-        s[i] -= 1;
-        const T& yl = GRID(s);
-
-        double weight = 1.0 / dx(GRID, i);
-        gradient[i] = weight * (yh - yl);
-    }
-    return gradient;
-}
-
-template <int dim, typename T> vector<T> downside_grad(const grid<dim, T>& GRID, const vector<int>& x)
-{
-    vector<T> gradient(dim);
-    vector<int> s = x;
-
-    for (int i=0; i<dim; i++) {
-        const T& yh = GRID(s);
-        s[i] -= 1;
-        const T& yl = GRID(s);
-		s[i] += 1;
-
-        double weight = 1.0 / dx(GRID, i);
-        gradient[i] = weight * (yh - yl);
-    }
-    return gradient;
-}
-
 template<typename T>
-double g(const MMSP::vector<T>& v)
+double multiwell(const MMSP::vector<T>& v)
 {
+	// this is the 'g' function, Eqn. 30
 	double ifce_nrg = 1.0/12;
 	for (int i=0; i<length(v); i++) {
-		ifce_nrg += pow(v[i],4)/4.0 - pow(v[i],3)/3.0;
+		ifce_nrg += pow(v[i],4.0)/4.0 - pow(v[i],3.0)/3.0;
 		for (int j=i+1; j<length(v); j++)
-			ifce_nrg += 0.5*pow(v[i],2)*pow(v[j],2);
+			ifce_nrg += pow(v[i],2.0)*pow(v[j],2.0)/2.0;
 	}
 	return ifce_nrg;
 }
@@ -160,54 +131,56 @@ void update(MMSP::grid<dim,vector<double> >& grid, int steps)
 			vector<int> x = position(grid,n);
 
 			vector<vector<double> > gradPhi = gradient(grid,x);
-			//vector<vector<double> > gradPhiU = upside_grad(grid,x);
-			//vector<vector<double> > gradPhiD = downside_grad(grid,x);
 			vector<double> lapPhi = laplacian(grid,x);
 
 			double denom = 0.0;
 			for (int i=0; i<fields(grid); i++) {
-				denom += pow(grid(x)[i],4);
+				denom += pow(grid(x)[i],4.0);
 				for (int j=i+1; j<fields(grid); j++)
-					denom += 2.0*pow(grid(x)[i],2)*pow(grid(x)[j],2);
+					denom += 2.0*pow(grid(x)[i],2.0)*pow(grid(x)[j],2.0);
 			}
 
 			double alleps = 0.0, allomg = 0.0;
 			for (int i=0; i<fields(grid); i++)
 				for (int j=0; j<fields(grid); j++) {
-					double gamij = energy(i,j);
-					double delij = width(i,j);
+					if (i==j) continue;
+					double gamij = 1.0; //energy(i,j);
+					double delij = 5.0; //width(i,j);
 					double epsij = 3.0*gamij*delij; // epsilon squared(ij)
 					double omgij = 3.0*gamij/delij; // omega(ij)
-					alleps += epsij*pow(grid(x)[i],2)*pow(grid(x)[j],2) / denom;
-					allomg += omgij*pow(grid(x)[i],2)*pow(grid(x)[j],2) / denom;
+					alleps += epsij*pow(grid(x)[i],2.0)*pow(grid(x)[j],2.0) / denom;
+					allomg += omgij*pow(grid(x)[i],2.0)*pow(grid(x)[j],2.0) / denom;
 				}
 
 			vector<double> dedp(fields(grid),0.0);
 			vector<double> dwdp(fields(grid),0.0);
 			vector<double> dgdp(fields(grid),0.0);
 			for (int i=0; i<fields(grid); i++) {
-				dgdp[i] = pow(grid(x)[i],3) - pow(grid(x)[i],2);
+				dgdp[i] = pow(grid(x)[i],3.0) - pow(grid(x)[i],2.0);
 				for (int j=0; j<fields(grid); j++) {
-					if (i<j)
-						dgdp[i] += grid(x)[i]*pow(grid(x)[j],2);
-					else if (i==j)
+					if (i==j)
 						continue;
-					double gamij = energy(i,j);
-					double delij = width(i,j);
+					else if (j>i)
+						dgdp[i] += grid(x)[i]*pow(grid(x)[j],2.0);
+					double gamij = 1.0; //energy(i,j);
+					double delij = 5.0; //width(i,j);
 					double epsij = 3.0*gamij*delij; // epsilon squared(ij)
 					double omgij = 3.0*gamij/delij; // omega(ij)
-					dedp[i] += 2.0*grid(x)[i]*(epsij - alleps)*pow(grid(x)[j],2) / denom;
-					dwdp[i] += 2.0*grid(x)[i]*(omgij - allomg)*pow(grid(x)[j],2) / denom;
+					dedp[i] += 2.0*grid(x)[i]*(epsij - alleps)*pow(grid(x)[j],2.0) / denom;
+					dwdp[i] += 2.0*grid(x)[i]*(omgij - allomg)*pow(grid(x)[j],2.0) / denom;
 				}
 			}
 
 			vector<double> dFdp(fields(grid),0.0);
 			double sumdFdp = 0.0;
 			for (int i=0; i<fields(grid); i++) {
-				dFdp[i] = allomg*dgdp[i] + g(grid(x))*dwdp[i] - alleps*lapPhi[i];
-				for (int j=0; j<fields(grid); j++)
+				dFdp[i] = allomg*dgdp[i] + multiwell(grid(x))*dwdp[i] - alleps*lapPhi[i];
+				for (int j=0; j<fields(grid); j++) {
+					if (i==j)
+						continue;
 					for (int d=0; d<dim; d++)
 						dFdp[i] += gradPhi[d][j] * (0.5*dedp[i]*gradPhi[d][j] - dedp[j]*gradPhi[d][i]);
+				}
 				sumdFdp += dFdp[i];
 			}
 
